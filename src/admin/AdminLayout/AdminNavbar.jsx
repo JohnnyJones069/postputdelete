b440759753290e5676149d18e5e2e59ab82e961e
Jonas Rishøj Christensen <jona623p@videndjurs.net>
2022-10-05 13:17:34 +0200

    Multiple link active

--- src/admin/AdminLayout/AdminNavbar.jsx
+++ src/admin/AdminLayout/AdminNavbar.jsx
@@ -8,7 +8,7 @@ const AdminNavbar = () => {
         <ul>
             {/* end tilføjet for at ungå at Home er .aktiv konstant */}
             <li><NavLink to="/admin">Admin Home</NavLink></li>
-            <li><NavLink to="/">Public Home</NavLink></li>
+            <li><NavLink to="/" end>Public Home</NavLink></li>
         </ul>
     </nav>
   )
